5584ceb5691900a6929b3f44768f8479c21298d7
Jesus Pineda <jesuspineda18@outlook.es>
2023-10-31 16:48:31 -0500

    add button for move to top page and icons social media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,40 @@ import Blog from "@/components/blog";
 import Clients from "@/components/clients/clients";
 import Contact from "@/components/contact/contact";
 import Services from "@/components/services";
-import { Button, Card, CardBody, Progress } from "@nextui-org/react";
-import Image from "next/image";
-import { useState } from "react";
 export default function Home() {
-  const testimonios = [
-    {
-      id: 1,
-      nombre: "Nombre del Cliente 1",
-      testimonio: "Testimonio del Cliente 1.",
-    },
-    {
-      id: 2,
-      nombre: "Nombre del Cliente 2",
-      testimonio: "Testimonio del Cliente 2.",
-    },
-    // Agrega más testimonios según sea necesario
-  ];
 
   return (
-    <div>
+    <div className="relative">
       <header className="container mx-auto flex justify-center items-center py-32">
-          <div className="flex flex-col gap-3 my-12 w-2/3">
-            <h1 className="font-black text-6xl text-center" >
-              Desarrollo de <span className="text-primary">software</span> y <span className="text-primary"> marketing digital </span>
-            </h1>
-            <p className="text-xl text-center">
-              En nuestra empresa estamos enfocados en la ayuda y satisfaccion de
-              todos nuestros clientes con soluciones adaptadas a cada una de sus
-              necesidades
-            </p>
-          </div>
+        <div className="flex flex-col gap-3 my-12 w-2/3">
+          <h1 className="font-black text-6xl text-center">
+            Desarrollo de <span className="text-primary">software</span> y{" "}
+            <span className="text-primary"> marketing digital </span>
+          </h1>
+          <p className="text-xl text-center">
+            En nuestra empresa estamos enfocados en la ayuda y satisfaccion de
+            todos nuestros clientes con soluciones adaptadas a cada una de sus
+            necesidades
+          </p>
+        </div>
       </header>
       <Services />
       <Blog />
-      <Clients/>
-      <Contact/>
+      <Clients />
+      <Contact />
+      {/* boton flotante abajo a la derecha para desplazarse hasta arriba */}
+      <div className="fixed bottom-0 right-0 mb-4 mr-4">
+        <button
+          onClick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          className="absolute bottom-3 right-3 bg-primary rounded-xl"
+        >
+          {/* icono en svg flecha hacia arriba */}
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="currentColor" d="m7 14l5-5l5 5H7Z"/></svg>
+            
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 
 export default function Footer() {
-
-  
   return (
     <div>
-      <footer className="text-white text-center p-3">
-        Desarrollado por Jp Development © 2023 - Todos los derechos reservados
+      <footer className="text-white text-center p-3 flex flex-col items-center gap-3">
+        <div className="flex gap-6">
+          <div>
+            <a href="https://www.facebook.com/JpDevelopmentCol" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"
+                  fill="currentColor"
+                />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <a href="https://www.instagram.com/jp_development/" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div>
+          Desarrollado por Jp Development © 2023 - Todos los derechos reservados
+        </div>
       </footer>
     </div>
   );
