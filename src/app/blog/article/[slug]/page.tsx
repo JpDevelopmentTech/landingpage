a2075532cd42716fcce9bd1bd013f7bb88d52619
Jesus Pineda <jesuspineda18@outlook.es>
2023-11-02 01:55:55 -0500

    add responsive

--- src/app/blog/article/[slug]/page.tsx
+++ src/app/blog/article/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default function Page({ params }: { params: { slug: string } }) {
   
   return (
     <div className="container mx-auto my-24 flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-1/2">
+      <div className="flex flex-col items-center justify-center md:w-1/2 text-justify md:text-left px-6">
         <Image src={article?.image} width={1280} height={720} className="w-full rounded-lg" alt={'Articulo ' + article?.title}/>
         <h1 className="text-center text-4xl font-bold my-12">{article?.title}</h1>
         <div dangerouslySetInnerHTML={{__html: article?.content}}></div>
