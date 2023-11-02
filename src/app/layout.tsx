fc4aedced7d7c9c663dec9e551e39d3ccc872660
Jesus Pineda <jesuspineda18@outlook.es>
2023-11-02 01:12:49 -0500

    fix problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 import { Providers } from "./provider";
+import { useEffect } from "react";
+import { app } from "@/services/firebase";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
+export const analytics = getAnalytics(app);
+
 
 export const metadata: Metadata = {
   title: "Jp Development | Desarrollo de Software",
@@ -19,6 +24,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    logEvent(analytics, 'notification_received')
+  }, [])
+
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
